96c6c5033f823828aae3d1d9aa465a502447b97c
Rohit Kumar <rohitkumar@uchicago.edu>
2025-08-18 15:18:48 -0500

    modified code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,13 @@
 # Run the tests with: pytest tests/test_app.py
+"""
+Unit test:
+
+"""
+
 from app import add_numbers
 
 def test_add_numbers():
+    """
+    Testing add_numbers function
+    """
     assert 3 == add_numbers(1,2)
